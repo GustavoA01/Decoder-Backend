dcdc100039a868c31f14b96873403090a633926c
GustavoA01 <gustavoaguiar14325@gmail.com>
2026-06-15 20:23:34 -0300

    correcao cors

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
-﻿from flask import Flask, jsonify
+﻿from flask import Flask, jsonify, make_response, request
 from routes.file import file_bp
 from routes.summary import summary_bp
 
 app = Flask(__name__)
 
 
-@app.after_request
-def add_cors_headers(response):
-    response.headers["Access-Control-Allow-Origin"] = "*"
+def _add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = request.headers.get("Origin", "*")
     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    response.headers["Access-Control-Max-Age"] = "86400"
     response.headers["Cache-Control"] = "no-cache"
     response.headers["X-Accel-Buffering"] = "no"
     return response
+
+
+@app.before_request
+def handle_preflight():
+    if request.method == "OPTIONS":
+        response = make_response("", 204)
+        return _add_cors_headers(response)
+
+    return None
+
+
+@app.after_request
+def add_cors_headers(response):
+    return _add_cors_headers(response)
 
 
 @app.route("/", methods=["GET"])
